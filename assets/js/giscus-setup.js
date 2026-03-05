ad679964dabc3ded044c8817718cf2cf6860d537
bcode0127-debug <bcode0127@gmail.com>
2026-03-05 14:19:25 +0000

    Deploying to gh-pages from @ bcode0127-debug/bcode0127-debug.github.io@0c3eb780bf830a5efa6563906d11a7f6fa0df65f 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "bcode0127-debug/bcode-debug.github.io",
+    "data-repo": "bcode0127-debug/bcode0127-debug.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
